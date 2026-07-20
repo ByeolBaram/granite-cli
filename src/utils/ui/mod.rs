a27c9160c5a09a44c5416d4d0f2a6d9a21242a86
Nishanth Kolakalapudi <Nishanth.Kol@ibm.com>
2026-07-20 16:26:37 -0500

    fix: add app module declaration missing from ui/mod.rs
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- src/utils/ui/mod.rs
+++ src/utils/ui/mod.rs
@@ -1,7 +1,9 @@
+pub mod app;
 pub mod backends;
 pub mod output;
 pub mod prompt;
 pub mod tui;
 
+pub use app::run_interactive_tui;
 pub use output::{CaptureOutput, Output, OUTPUT_REGISTRY};
 pub use prompt::prompt_from_schema;
